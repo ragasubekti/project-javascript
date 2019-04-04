320507edd22e07346519e9896d30d31d225ca939
Raga Subekti <rfilmriot@gmail.com>
2019-04-04 17:15:49 +0700

    Added marital status

--- scripts/index.js
+++ scripts/index.js
@@ -7,12 +7,14 @@ var User = /** @class */ (function () {
         this.addresss = "Jl. Raden Panji Suroso";
         this.gender = "Male";
         this.wife = undefined;
+        this.married = false;
     }
     User.prototype.sayHello = function () {
         console.log("Hello, my name is " + this.name);
-        console.log("'My age is " + this.age + "'");
+        console.log("My age is " + this.age);
         console.log("I live on " + this.addresss);
         console.log("My wife is " + this.wife);
+        console.log("Am i married? " + this.married);
         console.log("My github username is " + this.userName);
     };
     return User;
--- scripts/index.ts
+++ scripts/index.ts
@@ -7,6 +7,7 @@ class User {
   addresss: string;
   gender: string;
   wife: undefined;
+  married: boolean;
 
   constructor() {
     this.userName = "ragasubekti";
@@ -15,6 +16,7 @@ class User {
     this.addresss = "Jl. Raden Panji Suroso";
     this.gender = "Male";
     this.wife = undefined;
+    this.married = false;
   }
 
   sayHello() {
@@ -22,6 +24,7 @@ class User {
     console.log(`My age is ${this.age}`);
     console.log(`I live on ${this.addresss}`);
     console.log(`My wife is ${this.wife}`);
+    console.log(`Am i married? ${this.married}`);
     console.log(`My github username is ${this.userName}`);
   }
 }
