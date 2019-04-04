583970aeda30f959370f81200bb7b76430fde5b9
Raga Subekti <rfilmriot@gmail.com>
2019-04-04 17:14:16 +0700

    Add more types

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,18 @@
-"use strict";
-exports.__esModule = true;
+// export {};
 var User = /** @class */ (function () {
     function User() {
         this.userName = "ragasubekti";
         this.name = "Raga Subekti";
         this.age = 20;
         this.addresss = "Jl. Raden Panji Suroso";
+        this.gender = "Male";
+        this.wife = undefined;
     }
     User.prototype.sayHello = function () {
         console.log("Hello, my name is " + this.name);
         console.log("'My age is " + this.age + "'");
         console.log("I live on " + this.addresss);
+        console.log("My wife is " + this.wife);
         console.log("My github username is " + this.userName);
     };
     return User;
--- scripts/index.ts
+++ scripts/index.ts
@@ -1,22 +1,27 @@
-export {};
+// export {};
 
 class User {
   userName: string;
   name: string;
   age: number;
   addresss: string;
+  gender: string;
+  wife: undefined;
 
   constructor() {
     this.userName = "ragasubekti";
     this.name = "Raga Subekti";
     this.age = 20;
     this.addresss = "Jl. Raden Panji Suroso";
+    this.gender = "Male";
+    this.wife = undefined;
   }
 
   sayHello() {
     console.log(`Hello, my name is ${this.name}`);
-    console.log(`'My age is ${this.age}'`);
+    console.log(`My age is ${this.age}`);
     console.log(`I live on ${this.addresss}`);
+    console.log(`My wife is ${this.wife}`);
     console.log(`My github username is ${this.userName}`);
   }
 }
